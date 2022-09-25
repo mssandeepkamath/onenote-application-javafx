ff4c8a67a2147c47452efd5d14ee50736b7564cf
mssandeepkamath <msandeepcip@gmail.com>
2022-09-25 18:46:21 +0530

    new changes

--- src/main/java/com/onenote/onenote/ChooserController.java
+++ src/main/java/com/onenote/onenote/ChooserController.java
@@ -3,17 +3,32 @@ package com.onenote.onenote;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.AmbientLight;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class ChooserController {
     @FXML public Button notesListButton;
     @FXML public Button whiteBoardButton;
     @FXML public Button todoListButton;
+    @FXML
+    public Button logout;
+
+
+
+
+
+
+
 
     @FXML public void whiteBoardAction(ActionEvent actionEvent) throws IOException {
         Stage window = (Stage) whiteBoardButton.getScene().getWindow();
@@ -22,6 +37,7 @@ public class ChooserController {
         window.setMaximized(true);
         window.setResizable(true);
         window.getScene().setRoot(pane);
+
 
 
 
@@ -43,5 +59,14 @@ public class ChooserController {
         window.getScene().setRoot(pane);
 
 
+
+    }
+
+
+    public void logoutAction(ActionEvent actionEvent) throws IOException {
+        Stage window = (Stage) todoListButton.getScene().getWindow();
+        Parent pane = FXMLLoader.load(
+                Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        window.getScene().setRoot(pane);
     }
 }
